d95302491e2e280ba86e57a2e2e51d6d2e9d505e
Kateryna Kvasniak <kateryna.kvasniak@gmail.com>
2026-03-19 12:48:58 +0100

    rive-web to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/rive-web/',
   plugins: [react(), tailwindcss()],
 })
